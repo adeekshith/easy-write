83c0846cef7e2cb5e71bd1caebc13248599dec27
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-03-31 20:11:31 -0500

    refactor: Move JS in index.html to js/main.js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Deekshith Allamaneni on 3/31/16.
+ * Copyright Deekshith Allamaneni 2016
+ */
+//"use strict";
+
+function highlightTextAreaWithGivenWords(set1, set2, set3, set4, set5) {
+    $('textarea').highlightTextarea({
+            words: [
+                {
+                    words: ['browser', 'online'],
+                    color: "#FDD01C"
+                },
+                {
+                    words: ['ergonomics', 'malignant'],
+                    color: "#fab22b"
+                },
+                {
+                    words: ['quintessence'],
+                    color: "#f8943a"
+                },
+                {
+                    words: ['quantum'],
+                    color: "#f67749"
+                },
+                {
+                    words: ['javascript'],
+                    color: "#f35958"
+                }
+            ]
+        }
+    );
+}
+
+document.getElementById("easy-text-textarea").oninput=highlightTextAreaWithGivenWords([],[],[],[],[]);
