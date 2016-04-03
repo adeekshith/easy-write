49e99bcc6676fe3f2f26157a9f4d358b6291d12a
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-04-03 00:18:45 -0500

    feat: Select Pre Populated Textarea Text By Default

--- js/main.js
+++ js/main.js
@@ -2,34 +2,7 @@
  * Created by Deekshith Allamaneni on 3/31/16.
  * Copyright Deekshith Allamaneni 2016
  */
-//"use strict";
+"use strict";
 
-function highlightTextAreaWithGivenWords(set1, set2, set3, set4, set5) {
-    $('textarea').highlightTextarea({
-            words: [
-                {
-                    words: ['browser', 'online'],
-                    color: "#FDD01C"
-                },
-                {
-                    words: ['ergonomics', 'malignant'],
-                    color: "#fab22b"
-                },
-                {
-                    words: ['quintessence'],
-                    color: "#f8943a"
-                },
-                {
-                    words: ['quantum'],
-                    color: "#f67749"
-                },
-                {
-                    words: ['javascript'],
-                    color: "#f35958"
-                }
-            ]
-        }
-    );
-}
-
-//document.getElementById("easy-text-textarea").oninput=highlightTextAreaWithGivenWords([],[],[],[],[]);
+var easyWriteTextarea = document.getElementById("easy-write-textarea");
+easyWriteTextarea.setSelectionRange(0, easyWriteTextarea.value.length);
